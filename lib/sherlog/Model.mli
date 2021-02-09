a062a617667a7c3524236b5f48fcdea8a85bd430
Calvin Smith <calvin@Picante.attlocal.net>
2021-02-09 10:43:48 -0600

    examples

--- lib/sherlog/Model.mli
+++ lib/sherlog/Model.mli
@@ -1,3 +1,23 @@
+module Node : sig
+    type t = {
+        target : string;
+        guard : string;
+        parameters : Watson.Term.t list;
+    }
+end
+
+module Observation : sig
+
+end
+
+type t
+
+
+
+module JSON : sig
+    val encode : t -> JSON.t
+end
+
 type t
 
 val of_solutions : Watson.Proof.Solution.t list -> t
