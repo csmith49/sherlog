10f455e8594e2c9ae007be74f06b3c9f804d6187
Calvin Smith <calvin@wireless-10-146-30-15.public.utexas.edu>
2021-10-26 12:14:21 -0500

    fix: forgot to save merge result...

--- sherlog/explanation/explanation.py
+++ sherlog/explanation/explanation.py
@@ -1,11 +1,6 @@
 """Explanations are the central explanatory element in Sherlog. They capture sufficient generative constraints to ensure a particular outcome."""
 
-<<<<<<< HEAD
 from ..pipe import Pipeline, Statement
-=======
-from ..pipe import Pipeline, Semantics, Statement
-from ..interface import print
->>>>>>> 99820b2263b8b77b9a67ce22b16fa0a8150b5ce1
 from ..interface.instrumentation import minotaur
 from .semantics.core.target import EqualityIndicator
 from .semantics import spyglass
@@ -14,10 +9,6 @@ from .history import History
 
 from torch import Tensor, stack, tensor
 from typing import TypeVar, Mapping, Optional, Callable, List, Iterable
-<<<<<<< HEAD
-=======
-from itertools import chain
->>>>>>> 99820b2263b8b77b9a67ce22b16fa0a8150b5ce1
 
 T = TypeVar('T')
 
@@ -30,7 +21,6 @@ class Explanation:
         self.pipeline, self.observations, self.history = pipeline, observations, history
         self.locals = locals if locals else {}
 
-<<<<<<< HEAD
     # evaluation framework
 
     def target_stub(self) -> Iterable[Statement]:
@@ -71,68 +61,6 @@ class Explanation:
             results.append(result)
 
         # MC approx. of log-prob
-=======
-    # EVALUATION FRAMEWORK
-
-    def stub(self) -> Iterable[Statement]:
-        """Stub for combining observations."""
-
-        targets = [obs.target(key) for key, obs in enumerate(self.observations)]
-        yield Statement("sherlog:target", "max", targets)
-
-    @minotaur("evaluate")
-    def evaluate(self, parameters : Mapping[str, Tensor], semantics : Semantics[T], default=0.0) -> Mapping[str, T]:
-        """Evaluate the explanation."""
-
-        # run the generative process marginalized to the explanation
-        store = semantics(self.pipeline, parameters)
-
-        print(store)
-
-        # collect all further statements from the observation/explanation stubs
-        statements = chain(
-            *[obs.stub(key, default=default) for key, obs in enumerate(self.observations)],
-            self.stub()
-        )
-
-        # evaluate the statmenets
-        for statement in statements:
-            print(statement)
-            print(semantics.evaluate(statement, store))
-            store[statement.target] = semantics.evaluate(statement, store)
-
-        return store
-
-    @minotaur("explanation log-prob")
-    def log_prob(self, parameters : Mapping[str, Tensor], width : int = 1) -> Tensor:
-        """Compute the log-probability of the explanation generating the observations."""
-
-        # dyanmically construct the semantics and execute with the given parameters
-        semantics = spyglass.semantics_factory(
-            target=EqualityIndicator(),
-            locals=self.locals,
-            width=width
-        )
-        store = self.evaluate(parameters, semantics)
-
-        print(store["sherlog:target"])
-
-        # observation map
-        for index, observation in enumerate(self.observations):
-            result = store[f"sherlog:target:{index}"]
-            print(observation)
-
-        # PROBLEM IS HERE, AT LEAST IN PART
-        results = []
-        for index, _ in enumerate(self.observations):
-            # SHOULD BE MAX, NOT MEAN - EXPLAINS LOW LOG-PROB IN SUCCESSFUL BENCHMARKS?
-            result = stack([clue.surrogate for clue in store[f"sherlog:target:{index}"]]).max()
-            if result > 0:
-                results.append(result.log())
-
-        # CAN WE RETURN SOME VALUE OTHER THAN ZERO? NO WAY TO PROP GRADIENTS WITH DICE THIS WAY
-        # IDEA 1: CHANGE SCORE TO -1, 1 AND APPLY LINEAR TRANSFORMATION 0.5 X + 0.5 BEFORE TAKING LOG
->>>>>>> 99820b2263b8b77b9a67ce22b16fa0a8150b5ce1
         try:
             result = stack(results).mean().log()
         except RuntimeError:
