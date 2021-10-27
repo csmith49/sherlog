4468734973bd62017b9c2745e72aa0e8318cc5be
Calvin Smith <email@cjsmith.io>
2021-10-27 17:23:11 -0500

    coin flip performance restored

--- examples/coin-flip/evaluate.py
+++ examples/coin-flip/evaluate.py
@@ -10,6 +10,13 @@ from sherlog.program import loads
 from sherlog.inference import minibatch, PartitionEmbedding, Optimizer
 from sherlog.interface import print, initialize
 from sherlog.interface.instrumentation import minotaur
+from torch import tensor
+
+
+from rich import print
+from rich.table import Table
+
+
 
 SOURCE = \
 """
@@ -65,22 +72,34 @@ def cli(probability, train, batch_size, epochs, learning_rate, instrumentation, 
 
     optimizer = Optimizer(program, learning_rate=learning_rate, samples=samples)
 
+    old_batch_loss = tensor(0.0)
+
     # iterate over the data, and optimize
     for batch in minibatch(data, batch_size, epochs=epochs):
         with minotaur("batch"):
+            # we print out a "frame" for each batch for debugging purposes...
+            print(f"\n🪙 Batch {batch.index:03d} in Epoch {batch.epoch:03d} 🪙")
+
+            # what is the true probability suggested by the batch?
+            head_count = len(list(filter(None, batch.data)))
+            batch_probability = head_count / len(batch.data)
+            print(f"Batch P: {batch_probability:.2f}")
+
+            # okay, now let's optimize
             optimizer.maximize(*embedder.embed_all(batch.data))
             batch_loss = optimizer.optimize()
 
+            # what is the batch loss?
+            batch_loss_delta = old_batch_loss - batch_loss
+            print(f"Batch loss: {batch_loss:.3f} (Δ {batch_loss_delta:.3f})")
+            old_batch_loss = batch_loss
+
+            print(f"P: {program._parameters[0].value.item():.3f} (error: ±{abs(program._parameters[0].value.item() - probability):.3f})")
+            # and what is the resulting gradient for the probability?
+            print(f"∇P: {program._parameters[0].value.grad.item():.3f}")
+
             minotaur["batch"] = batch.index
             minotaur["epoch"] = batch.epoch
-
-            print(f"Batch {batch.index}:{batch.epoch} loss: {batch_loss.item()}.")
-
-    # report the parameter
-    p = program._parameters[0]
-    minotaur["p"] = p.value.item()
-
-    print(f"Resulting parameter p: {p.value}")
 
     minotaur.exit()
 
