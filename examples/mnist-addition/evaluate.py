b0b40ee294e505266b43a0c152c78494b7863a53
Calvin Smith <email@cjsmith.io>
2021-11-02 11:07:39 -0500

    feat: fully annotated mnist-addition, no convergence witnessed so far

--- examples/mnist-addition/evaluate.py
+++ examples/mnist-addition/evaluate.py
@@ -1,29 +1,28 @@
-"""Sherlog example: MNIST digit addition."""
+"""
+"""
 
 import click
 
-from random import choice
 from sherlog.program import loads
-from sherlog.inference import minibatch, FunctionalEmbedding, Optimizer
+from sherlog.inference import minibatch, Optimizer, FunctionalEmbedding
 from sherlog.interface import print, initialize
-from sherlog.interface.instrumentation import minotaur
 
-from torch import nn, argmax
+from random import choice
+from torch import nn, tensor, argmax
 
 import torchvision
 import torchvision.transforms as transforms
 
 SOURCE = \
 """
-digit_weights(X; digit_nn[X]).
-digit(X; categorical[W]) <- digit_weights(X, W).
-
-addition(X, Y; add[X', Y']) <- digit(X, X'), digit(Y, Y').
+digit(X; {0, 1, 2, 3, 4, 5, 6, 7, 8, 9} <- digit_nn[X]).
+addition(X, Y; add[X, Y]).
+observe(X, Y, Z) <- digit(X, X'), digit(Y, Y'), addition(X', Y', Z).
 """
 
-class MNISTModule(nn.Module):
+class MNISTClassifier(nn.Module):
     def __init__(self):
-        super(MNISTModule, self).__init__()
+        super(MNISTClassifier, self).__init__()
 
         self.encoder = nn.Sequential(
             nn.Conv2d(1, 6, 5),
@@ -59,30 +58,36 @@ MNIST = torchvision.datasets.MNIST(
 )
 
 def sample():
-    x, xi = choice(MNIST)
-    y, yi = choice(MNIST)
+    left_image, left_digit = choice(MNIST)
+    right_image, right_digit = choice(MNIST)
+    return {
+        "left_image" : left_image,
+        "right_image" : right_image,
+        "left_digit" : left_digit,
+        "right_digit" : right_digit
+    }
 
-    return (x, y, xi + yi)
+def classification_accuracy(classifier, data):
+    correct = 0
+    for datum in data:
+        # predict left
+        prediction = argmax(classifier(datum["left_image"]))
+        if prediction == datum["left_digit"]:
+            correct += 1
+        # predict right
+        prediction = argmax(classifier(datum["right_image"]))
+        if prediction == datum["right_digit"]:
+            correct += 1
+    return correct / (len(data) * 2)
 
 @click.command()
 @click.option("--train", default=100, type=int, help="Number of training samples.")
-@click.option("--test", default=100, type=int, help="Number of testing samples.")
 @click.option("-b", "--batch-size", default=10, type=int, help="Training minibatch size.")
 @click.option("-e", "--epochs", default=10, type=int, help="Training epochs.")
 @click.option("-l", "--learning-rate", default=1e-4, type=float, help="Learning rate.")
-@click.option("-i", "--instrumentation", type=str, help="Filepath to dump instrumentation logs.")
-def cli(train, test, batch_size, epochs, learning_rate, instrumentation):
-    
-    # do this first, or we lose some initial messages
-    if instrumentation:
-        minotaur.add_filepath_handler(instrumentation)
-
-    minotaur.enter("mnist addition")
-
-    minotaur["train"] = train
-    minotaur["batch size"] = batch_size
-    minotaur["epochs"] = epochs
-    minotaur["learning rate"] = learning_rate
+@click.option("-s", "--samples", default=100, type=int, help="Number of per-datum samples.")
+def cli(train, batch_size, epochs, learning_rate, samples):
+    """Learn the parameters for a simple MNIST digit classifier via addition observations."""
 
     # initialize!
     print("Initializing...")
@@ -90,55 +95,49 @@ def cli(train, test, batch_size, epochs, learning_rate, instrumentation):
 
     # load the program
     print("Loading the program...")
-    program, _ = loads(SOURCE, locals={
-        "digit_nn" : MNISTModule()
-    })
+    program, _ = loads(SOURCE, locals={"digit_nn" : MNISTClassifier()})
 
     # load the data
     print(f"Generating {train} training points...")
 
-    with minotaur("data generation"):
-        data = [sample() for _ in range(train)]
+    data = [sample() for _ in range(train)]
 
-        embedder = FunctionalEmbedding(
-            evidence=lambda p: f"addition(left, right, {p[-1]})",
-            parameters=lambda p: {
-                "left" : p[0],
-                "right" : p[1]
-            }
-        )
+    embedder = FunctionalEmbedding(
+        evidence=lambda s: f"observe(left, right, {s['left_digit'] + s['right_digit']})",
+        parameters=lambda s: {
+            "left" : s["left_image"],
+            "right" : s["right_image"]
+        }
+    )
 
     # build the optimizer
     print(f"Initializing the optimizer with a learning rate of {learning_rate}...")
-    optimizer = Optimizer(program, learning_rate=learning_rate)
+    optimizer = Optimizer(
+        program=program,
+        samples=samples,
+        learning_rate=learning_rate
+    )
 
-    # iterate over the data, and optimize
+    # iterate over the data and optimize
+    old_batch_loss = tensor(0.0)
+
     for batch in minibatch(data, batch_size, epochs=epochs):
-        with minotaur("batch"):
-            optimizer.maximize(*embedder.embed_all(batch.data))
-            batch_loss = optimizer.optimize()
+        # we print out a frame for each batch for debugging purposes...
+        print(f"\n✏️ Batch {batch.index:03d} in Epoch {batch.epoch:03d} ✏️")
 
-            minotaur["batch"] = batch.index
-            minotaur["epoch"] = batch.epoch
+        # okay, now let's optimize
+        optimizer.maximize(*embedder.embed_all(batch.data))
+        batch_loss = optimizer.optimize()
 
-            print(f"Batch {batch.index}:{batch.epoch} loss: {batch_loss.item()}.")
+        # what is the batch loss?
+        print(f"Batch loss: {batch_loss:.3f} (Δ={old_batch_loss - batch_loss:.3f})")
 
+        # what is the network doing wrt accuracy?
+        classifier = program._locals["digit_nn"]
+        accuracy = classification_accuracy(classifier, batch.data)
+        print(f"Batch accuracy: {accuracy:0.3f}")
 
-    # check the accuracy of the digit classifier
-    correct = 0
-    for _ in range(test):
-        image, d = choice(MNIST)
-        prediction = program._locals["digit_nn"](image)
-        result = argmax(prediction).item()
-
-        print(f"{prediction}, {result}, {d}")
-
-        if result == d:
-            correct += 1
-
-    print(correct / test)
-    
-    minotaur.exit()
+        old_batch_loss = batch_loss
 
 if __name__ == "__main__":
     cli()
